a5de417e560c1334fda5d850702022a9d1d53728
angelakotora <angelakotora@gmail.com>
2018-09-23 14:55:32 -0400

    second commit

--- src/Lab1.java
+++ src/Lab1.java
@@ -5,39 +5,40 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//this is my second commit
  int num1;
  int num2;
- int sumNumbers;
- Scanner scan= new Scanner (System.in);
- System.out.println("Enter three digit number");
  
- num1 = scan.nextInt(); //enter number
- System.out.println("Enter three digit number");
- num2 = scan.nextInt(); //enter another number
+ Scanner scan= new Scanner(System.in);
+ System.out.println("Enter three digit number"); //Enter no more than three digits
+ num1 = scan.nextInt(); //prompt to enter number
+ System.out.println("Enter three digit number"); //Enter another set of three digits
+ num2 = scan.nextInt(); //prompt to enter another number
  
- sumNumbers = num1 + num2; //add the same
+ if (num1 > 999)
+		System.out.println("\nMust be more then 3 digits.");// Must be 3 digits
+ if (num2 < 100)
+		System.out.println("Must be less then 3 digits.");// Must be 3 digits
  
- int hundreds1 = (num1 / 100)%10;
+ int hundreds1 = (num1 / 100)%10; //first set for mathematical reasoning
  int tens1 = (num1 / 10)%10;
  int ones1 = num1 % 10;
  
- int hundreds2 = (num2 / 100)%10;
- int tens2 = (num2 / 100)%10;
+ int hundreds2 = (num2 / 100)%10; //second set for mathematical reasoning
+ int tens2 = (num2 / 10)%10;
  int ones2 = num2 % 10;
  
- int sumHundreds = hundreds1 + hundreds2;
+ int sumHundreds = hundreds1 + hundreds2; //int additions
  int sumTens = tens1 + tens2;
  int sumOnes = ones1 + ones2;
  
- int returnNum = sumHundreds + sumTens + sumOnes;
  
- if (sumHundreds == sumTens) {
-	 System.out.println(true);
-	if (sumTens == sumOnes)
-		System.out.println(true);
- } else {
-	 System.out.println(false);
- }
+ 
+ if (sumHundreds == sumTens&sumTens == sumOnes){
+     System.out.println(true); //numbers print true
+} else {
+  System.out.println(false); //numbers print false
+}
 	}
 
 }
--- src/Lab2.java
+++ src/Lab2.java
@@ -1,36 +1,37 @@
 //Angela Kotora
 import java.util.Scanner;
 public class Lab2 {
-
+    //this is my second commit
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		int oneDay = sc.nextInt();
+		Scanner scnr = new Scanner(System.in); //prompt for user input
+		int oneDay = scnr.nextInt(); //Prompt to enter 1st day
 		System.out.println("Enter earlier Day");
-		int oneMonth = sc.nextInt();
+		int oneMonth = scnr.nextInt(); // Prompt to enter 1st Month
 		System.out.println("Enter earlier Month");
-		int oneYear = sc.nextInt();
+		int oneYear = scnr.nextInt(); // Prompt to enter 1st Year
 		System.out.println("Enter earlier Year");
 		
-		int twoDay = sc.nextInt();
+		int twoDay = scnr.nextInt(); //Prompt to enter 2nd day
 		System.out.println("Enter later Day");
-		int twoMonth = sc.nextInt();
+		int twoMonth = scnr.nextInt(); //Prompt to enter 2nd Month
 		System.out.println("Enter later Month");
-		int twoYear = sc.nextInt();
+		int twoYear = scnr.nextInt(); // Prompt to enter 2nd Year
 		System.out.println("Enter later Year");
 		
-		int earlierDate1 = (oneYear *365 + oneMonth * 30)+ oneDay;
-		int earlierDate2 = (twoYear *365 + twoMonth * 30)+ twoDay;
+		int earlierDate1 = (oneYear *365 + oneMonth * 30)+ oneDay; //Yr one calculations
+		int earlierDate2 = (twoYear *365 + twoMonth * 30)+ twoDay; //Yr two calculations
 		
-		int dateDiff = earlierDate2 - earlierDate1;
-		int day = (dateDiff % 365)% 30;
-		int month = (dateDiff % 365) / 30;
-		int year = dateDiff / 365;
-		System.out.println("Days difference between:" + day + "-" + month + "=" + year + "day(s)");
-        //shows the difference in the number of days
+		int dateDiff = earlierDate2 - earlierDate1; //calculation getting the difference in dates
+		int day = (dateDiff % 365)% 30; //calculation btw the two dates
+		int month = (dateDiff % 365) / 30; //calculation btw the two months
+		int year = dateDiff / 365; //calculation btw the two years
+		
+		System.out.println("Days difference between:" + earlierDate2 + "-" + earlierDate1 + "=" + dateDiff + "day(s)");
+		//shows the difference in the number of days
 				
-			
+		return;	
 			
 		
 
